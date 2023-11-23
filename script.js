4a51553028b60c73d864e65982dfa09dd3abb1b9
Cristian727 <cristian.gonzalez7@hotmail.com>
2023-11-23 16:34:39 +0100

    Primeros 4 audios click

--- script.js
+++ script.js
@@ -8,7 +8,25 @@ for(let i = 0; i < pads.length; i++){
     audioMap[pads[i].innerHTML.toLowerCase()] = audio
 
     pads[i].addEventListener("click", function(){
-      console.log("pulsaste el pad", pads[i])
-      console.log("Hola " + pads[i].innerHTML)
+      //console.log("pulsaste el pad", pads[i])
+      if(audio){
+        playAudio(audio)
+    }
+      console.log("Pulsaste " + pads[i].innerHTML);
+      const key = pads[i].innerHTML.toLowerCase();
+    //document.querySelector("key").value = key;
+        pads[i].style.background = "mediumspringgreen"
+        setTimeout(function(){
+           pads[i].style.background = "lightgray"
+           pads[i].style.boxShadow = "0px 5px 0px lightblue"
+        },100)
+
+
     })
+}
+
+function playAudio(audio){
+    audio.pause()
+    audio.currentTime = 0;
+    audio.play()
 }
